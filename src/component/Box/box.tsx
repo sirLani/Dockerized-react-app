894a193e018f8ff43cfcd8eb5711ad9006b15e9c
olaide olanipekun <olaide.olanipekun@yahoo.com>
2021-12-09 03:26:05 +0000

    first commit

---
+++ src/component/Box/box.tsx
@@ -0,0 +1,14 @@
+﻿import React, { ReactNode } from "react";
+import styles from './Box.module.scss'
+
+export const Box: React.FC<{ children: ReactNode }> = ({ children }) => {
+  return <div className={styles.Box}>{children}</div>;
+};
+
+// interface Props {
+//   text: string;
+// }
+
+// export const Box: React.FC<Props> = () => {
+//   return <></>;
+// };
